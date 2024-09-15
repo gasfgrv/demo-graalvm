3cef0ba44a0a402a5bf7c89002052ff4b17ce94d
Gustavo Silva <gustavoalmeidasilva41@gmail.com>
2024-09-15 00:18:21 -0300

    Configuração em multi-módulos

--- app/src/main/java/com/github/gasfgrv/demo_graalvm/DemoGraalvmApplication.java
+++ app/src/main/java/com/github/gasfgrv/demo_graalvm/DemoGraalvmApplication.java
@@ -3,9 +3,11 @@ package com.github.gasfgrv.demo_graalvm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableFeignClients
-@SpringBootApplication
+@EnableFeignClients(basePackages = "com.github.gasfgrv.demo_graalvm")
+@SpringBootApplication(scanBasePackages = "com.github.gasfgrv.demo_graalvm")
+@ComponentScan(basePackages = "com.github.gasfgrv.demo_graalvm")
 public class DemoGraalvmApplication {
 
     public static void main(String[] args) {
